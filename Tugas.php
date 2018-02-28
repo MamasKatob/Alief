2023512aff7dc65a3ff7288b63c0cedb48687f17
Mamaskatob <bumiharja56@gmail.com>
2018-02-28 12:22:54 +0700

    commit tugas

---
+++ Tugas.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama  = "Muhammad Dimas Alief Bakhtiar";
+$usia  = "18";
+$kelas = "4C";
+$NIM   = "16090064";
+
+echo "$nama <br>";
+echo "$usia <br>";
+echo "$kelas <br>";
+echo "$NIM <br>";
+
+?>
